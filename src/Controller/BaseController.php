b9b99fbf25f1929cb878b2e2c8c581745d15425d
Michael <gougammichael@gmail.com>
2025-06-18 17:06:49 +0200

    mise ajour

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BaseController extends AbstractController
+{
+    #[Route('/', name: 'app_accueil')]
+    public function index(): Response
+    {
+        return $this->render('base/index.html.twig', [
+            
+        ]);
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+    return $this->render('base/contact.html.twig', [
+   
+    ]);
+    }
+    #[Route('/devis', name: 'app_devis')]
+    public function devis(): Response
+    {
+    return $this->render('base/devis.html.twig', [
+   
+    ]);
+    }
+    #[Route('/p', name: 'app_propos')]
+    public function propos(): Response
+    {
+    return $this->render('base/propos.html.twig', [
+   
+    ]);
+    }
+   
+}
